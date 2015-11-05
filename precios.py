7ba0db281535dbe65f8fed9780d662a5eb52fabe
01101010o <01101010o@openmailbox.org>
2015-11-05 19:33:19 -0300

    lista de precios

---
+++ precios.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+##...Creado por 01101010o...##
+##Lista de precios para la venta:
+##NOTA: para editar algun precio solo editar este documento.
+##NOTA2: para agregar o quitar productos, se requiere editar todos los archivos.
+carne=800
+pollo=800
+dobladitas=300
+dqueso=500
+chaparritas=600
+baño=200
